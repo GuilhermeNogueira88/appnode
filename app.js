af1c118188fe0e65d5d35e5b8608c5602be6427b
GUILHERME NOGUEIRA FERREIRA <guilherme.nferreira2@senacsp.edu.br>
2019-07-05 22:19:35 -0300

    consulta, delete alunos

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users');
+var produtosRouter = require('./routes/produtos');
+var alunosRouter = require('./routes/alunos');
+
+
 
 var app = express();
 
@@ -21,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/produtos', produtosRouter);
+app.use('/alunos', alunosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
