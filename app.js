686c1ed6ee91522a312f2bf48b599dead8824c50
GUILHERME NOGUEIRA FERREIRA <guilherme.nferreira2@senacsp.edu.br>
2019-07-22 22:09:10 -0300

    version 3.0

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var usersRouter = require('./routes/users');
 var usersRouter = require('./routes/users');
 var produtosRouter = require('./routes/produtos');
 var alunosRouter = require('./routes/alunos');
-
+var alunosNewRouter = require('./routes/alunosnew');
 
 
 var app = express();
@@ -28,7 +28,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/produtos', produtosRouter);
 app.use('/alunos', alunosRouter);
-
+app.use('/alunos/new', alunosNewRouter);
+app.use('alunos/edit', alunosNewRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ routes/alunosnew.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next){
+res.render('alunosnew', {title: 'Novo Aluno', alunos:{}});
+})
+
+router.get('/', function(req, res, next){
+    var id = req.body.id;
+    var nome = req.body.nome;
+    var idade = req.body.idade;
+    var db = require("../db");
+    if(id != ""){
+      db.updateAlunos(id, {nome, idade}, function(err,result){
+
+      })
+    } else{
+        db.insertAlunos({nome, idade}, function(err,result){
+            if(err) return console.log(err);
+            res.redirect('alunos/all');
+        })
+    }
+})
+module.exports = router;
